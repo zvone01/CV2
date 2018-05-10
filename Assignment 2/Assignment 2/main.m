07bd68db1f7350362bff7779b7f653881e29ffe6
knurpsbram <bramkooiman94@gmail.com>
2018-05-10 12:45:31 -0400

    dense submatrices within sparse matrix

--- Assignment 2/Assignment 2/main.m
+++ Assignment 2/Assignment 2/main.m
@@ -1,21 +1,21 @@
-%run('/usr/local/MATLAB/vlfeat/toolbox/vl_setup')
-run('C:\Users\zdujmic\Documents\MATLAB\vlfeat\toolbox\vl_setup')
-%im1 = im2single(imread('Data/House/House/frame00000001.png'));
-%im2 = im2single(imread('Data/House/House/frame00000002.png'));
+run('/usr/local/MATLAB/vlfeat/toolbox/vl_setup')
+% run('C:\Users\zdujmic\Documents\MATLAB\vlfeat\toolbox\vl_setup')
+im1 = im2single(imread('Data/House/House/frame00000001.png'));
+im2 = im2single(imread('Data/House/House/frame00000002.png'));
 
 %F = get_fundamental_mat(im1, im2, 'show');
-%F = get_fundamental_mat(im1, im2, 'norm', 'show');
+F = get_fundamental_mat(im1, im2, 'norm', 'show');
 %[F,c] = get_fundamental_mat(im1, im2, 'norm', 'show', 'ransac');
 
-%points from images
-PointViewMat = get_pointview_mat('Data/House/House/');
-[m, s] = sfm(PointViewMat);
-figure(1);
-plot3(s(1, :), s(2,:), s(3,:),'k.');
-
-
-%points from PointViewMatrix.txt
-pvm =importdata('PointViewMatrix.txt');
-[m, s] = sfm(pvm);
-figure(2);
-plot3(s(1, :), s(2,:), s(3,:),'k.');
+% % points from images
+% PointViewMat = get_pointview_mat('Data/House/House/');
+% [m, s] = sfm(PointViewMat);
+% figure(1);
+% plot3(s(1, :), s(2,:), s(3,:),'k.');
+% 
+% 
+% %points from PointViewMatrix.txt
+% pvm = importdata('PointViewMatrix.txt');
+% [m, s] = sfm(pvm);
+% figure(2);
+% plot3(s(1, :), s(2,:), s(3,:),'k.');
--- Assignment 2/Assignment 2/normalise.m
+++ Assignment 2/Assignment 2/normalise.m
@@ -10,11 +10,15 @@ d = mean( sqrt( (c(1,:,:) - repmat(mx,[1, n, 1])).^2 + (c(2,:,:) - repmat(my,[1,
 
 % transformation matrix (tensor)
 T = [ ones(1,1,2)*sqrt(2)./d zeros(1,1,2) -mx*sqrt(2)./d ; 
-    zeros(1,1,2) ones(1,1,2)*sqrt(2)./d -mx*sqrt(2)./d ; 
+    zeros(1,1,2) ones(1,1,2)*sqrt(2)./d -my*sqrt(2)./d ; 
     zeros(1,1,2) zeros(1,1,2) ones(1,1,2) ];
 
 % normalise
 c(:,:,1) = T(:,:,1)*c(:,:,1);
 c(:,:,2) = T(:,:,2)*c(:,:,2);
+
+mx_check = mean(c(1,:,:), 2)
+my_check = mean(c(2,:,:), 2)
+d_check = mean( sqrt( (c(1,:,:) - repmat(mx_check,[1, n, 1])).^2 + (c(2,:,:) - repmat(my_check,[1, n, 1])).^2 ), 2 )
 
 end
